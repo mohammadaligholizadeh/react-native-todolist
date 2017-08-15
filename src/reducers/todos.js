4c8ba52025a9c4edb9aed1fdfe2188be3890e0d0
mohamad.agz <mohamad.agz@gmail.com>
2017-08-16 01:58:29 +0430

    add reducers

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,49 +1,23 @@
-const todo = (state, action) => {
-    switch (action.type) {
-        case 'ADD_TODO':
-            return {
-                id: action.id,
-                title: action.title,
-                createdAt: new Date(),
-                completedAt: null,
-                completed: false
-            };
-        case 'TOGGLE_TODO':
-            if (state.id !== action.id) {
-                return state;
-            }
-
-            return Object.assign({}, state, {
-                completed: !state.completed,
-                completedAt: !state.completed ? new Date() : null
-            });
-        default:
-            return state;
-    }
-};
-
-const defaultTodos = [
-    {id: 1, title: 'Clean up garage', createdAt: new Date(), completed: false},
-    {id: 2, title: 'Install BBQ', createdAt: new Date(), completed: false},
-    {id: 3, title: 'Watch NBA final', createdAt: new Date(), completed: false},
-    {id: 4, title: 'Buy supprise egg', createdAt: new Date(), completed: false},
-    {id: 5, title: 'Watch Iron Man 3', createdAt: new Date(), completed: false},
-];
-
-const todos = (state = defaultTodos, action) => {
+const todos = (state = [], action) => {
     switch (action.type) {
         case 'ADD_TODO':
             return [
                 ...state,
-                todo(undefined, action)
-            ];
+                {
+                    id: action.id,
+                    text: action.text,
+                    completed: false
+                }
+            ]
         case 'TOGGLE_TODO':
-            return state.map(t =>
-                todo(t, action)
-            );
+            return state.map(todo =>
+                (todo.id === action.id)
+                    ? {...todo, completed: !todo.completed}
+                    : todo
+            )
         default:
-            return state;
+            return state
     }
-};
+}
 
-export default todos;
+export default todos
