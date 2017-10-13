ee5a6b7dd7f411d635af60ecce3620685b8739c4
mohamad.agz <mohamad.agz@gmail.com>
2017-10-13 21:17:30 +0330

    todo app reducer

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,13 +1,13 @@
-const todo = (state=[], action) => {
+const todo = (state={todoList:{}}, action) => {
+    const todoList = { ...state.todoList };
     switch (action.type) {
         case 'ADD_TODO':
-            return [
-                ...state,
-                action.text
-            ]
+            const newId = Object.keys(todoList).length;
+            todoList[`todo${newId}`] = {text:action.text , time: new Date()};
+            return { ...state, todoList };
         default:
             return state
     }
-}
+};
 
 export default todo;
